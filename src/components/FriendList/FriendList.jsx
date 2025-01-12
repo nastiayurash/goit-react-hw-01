cded9507b7361c5fed5f92c4280f59b3d74a7a0b
Nastia Yurash <nasstyayurash@gmail.com>
2025-01-12 06:07:12 +0200

    change file structure

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import FriendListItem from "./FriendListItem";
+import FriendListItem from "../FriendListItem/FriendListItem";
 import styles from "./FriendList.module.css";
 
 const FriendList = ({ friends }) => {
